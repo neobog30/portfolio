395083e212b2bdd7b7c7f7ec64e3829fc3ef4ad3
neobog30 <neobog30@gmail.com>
2022-02-03 09:58:19 +0200

    new component

---
+++ src/components/snippettest.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type SnippettestProps = {
+
+}
+
+const Snippettest: React.FC<SnippettestProps> = () => {
+  return (
+    <div>
+      Snippettest
+    </div>
+  )
+}
+
+export default Snippettest
