be23c52f876bf141ff136d68f0b99c950ed17646
neobog30 <neobog30@gmail.com>
2022-02-03 10:24:36 +0200

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SomeExampleComponent from './components/example/someExampleComponent';
 import MyFirstComponent from './components/myFirstComponent';
 
 function App() {
   return (
     <div className="App">
      <MyFirstComponent/>
+     <SomeExampleComponent/>
     </div>
   );
 }
